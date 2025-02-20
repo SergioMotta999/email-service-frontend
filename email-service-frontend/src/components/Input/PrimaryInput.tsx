99f214d5118c9066f47ae8b52549ee71c721d9bd
Sergio Mota <serginhomota77@gmail.com>
2025-02-20 20:45:47 -0300

    comitando pacotes para garantir consistencia nas dependencias

---
+++ email-service-frontend/src/components/Input/PrimaryInput.tsx
@@ -0,0 +1,24 @@
+import { Input } from "@chakra-ui/react";
+import { ChangeEventHandler } from "react";
+
+interface PrimaryInputProps {
+  name: string;
+  value: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+}
+
+export default function PrimaryInput({
+  name,
+  value,
+  onChange,
+}: PrimaryInputProps) {
+  return (
+    <Input
+      variant="filled"
+      placeholder="Filled"
+      name={name}
+      value={value}
+      onChange={onChange}
+    />
+  );
+}
