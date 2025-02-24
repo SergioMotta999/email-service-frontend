a93ee78389e82321e09e7d9d76a1fb6b2040b87c
Sergio Mota <serginhomota77@gmail.com>
2025-02-24 19:19:08 -0300

    Adicionando inputs na tela

--- email-service-frontend/src/App.tsx
+++ email-service-frontend/src/App.tsx
@@ -4,15 +4,30 @@ import PrimaryInput from "./components/Input/PrimaryInput";
 
 function App() {
   const [email, setEmail] = useState("");
+  const [firstName, setfirstName] = useState("");
+  const [secondName, setsecondName] = useState("");
 
   return (
-    <>
+    <div className="container">
+      <PrimaryInput
+        value={firstName}
+        onChange={(event) => setfirstName(event.target.value)}
+        name="email"
+        label="Digite seu e-mail"
+      />
+      <PrimaryInput
+        value={secondName}
+        onChange={(event) => setsecondName(event.target.value)}
+        name="email"
+        label="Digite seu e-mail"
+      />
       <PrimaryInput
         value={email}
         onChange={(event) => setEmail(event.target.value)}
         name="email"
+        label="Digite seu e-mail"
       />
-    </>
+    </div>
   );
 }
 
--- email-service-frontend/src/components/Input/PrimaryInput.tsx
+++ email-service-frontend/src/components/Input/PrimaryInput.tsx
@@ -1,24 +1,30 @@
 import { Input } from "@chakra-ui/react";
 import { ChangeEventHandler } from "react";
+import "./primary-input..css";
 
 interface PrimaryInputProps {
   name: string;
   value: string;
   onChange: ChangeEventHandler<HTMLInputElement>;
+  label: string;
 }
 
 export default function PrimaryInput({
   name,
   value,
   onChange,
+  label,
 }: PrimaryInputProps) {
   return (
-    <Input
-      variant="filled"
-      placeholder="Filled"
-      name={name}
-      value={value}
-      onChange={onChange}
-    />
+    <div className="input-container">
+      <label className="label">{label}</label>
+      <Input
+        variant="filled"
+        placeholder="Digite seu e-mail"
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
   );
 }
